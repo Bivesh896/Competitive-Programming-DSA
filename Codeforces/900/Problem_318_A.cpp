c85eaaccb79990796f96dd2f6f9a4289d51faa25
Bivesh896 <dalaibivesh@gmail.com>
2026-06-23 23:03:40 +0530

    Add files via upload

---
+++ Codeforces/900/Problem_318_A.cpp
@@ -0,0 +1,21 @@
+/*
+Problem: 318-A Even Odds
+Platform: Codeforces
+Rating: 900
+Topic: Math 
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long int n,k;
+    cin>>n>>k;
+    if(k<=((n+1)/2))
+        cout<<2*k-1;
+    else
+    {
+        k = k-((n+1)/2);
+        cout<<2*k;
+    }
+    return 0;
+}
